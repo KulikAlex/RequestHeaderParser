ed1c685a14cbf5ce249f93577165e8f0b7959e76
System <none>
2017-06-10 15:43:11 +0000

    Updated with Glitch

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get("/", function (request, response) {
+  var result = {ipaddress:request.headers["x-forwarded-for"].split(",")[0],
+               language:request.headers["accept-language"].split(",")[0],
+               software:request.headers["user-agent"].split("(")[1].split(")")[0]};
+  response.send(result);
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
